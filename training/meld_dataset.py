2f0b7ce7fad851aba787654d70e94eb50fe499f5
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-02 19:17:31 +0530

    added emotion and sentiment mapping to MELDDataset class and initialized BERT tokenizer

--- training/meld_dataset.py
+++ training/meld_dataset.py
@@ -1,11 +1,35 @@
 from torch.utils.data import Dataset
 import pandas as pd
+from transformers import AutoTokenizer
 
 
 class MELDDataset(Dataset):
     def __init__(self, csv_path, video_dir):
         self.data = pd.read_csv(csv_path)  # read the csv file
-        print(len(self.data))
+
+        self.video_dir = video_dir  # directory containing the videos
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            # loading the tokenizer which allows us to use the BERT model and convert the text to tokens
+            "bert-base-uncased")
+
+        # adding emotion labels to the tokenizer
+        self.emotion_map = {
+            'anger': 0,
+            'disgust': 1,
+            'fear': 2,
+            'joy': 3,
+            'neutral': 4,
+            'sadness': 5,
+            'surprise': 6
+        }
+
+        # adding sentiment labels to the tokenizer
+        self.sentiment_map = {
+            'negative': 0,
+            'neutral': 1,
+            'positive': 2
+        }
 
 
 if __name__ == "__main__":
