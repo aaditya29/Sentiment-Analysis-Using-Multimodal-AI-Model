fee212587fadbee355c6556bdd8ed0b3e4a62e4e
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-02 19:09:08 +0530

    tested data import

--- training/meld_dataset.py
+++ training/meld_dataset.py
@@ -1,10 +1,13 @@
 from torch.utils.data import Dataset
+import pandas as pd
 
 
 class MELDDataset(Dataset):
-    def __init__(self):
-        print("Hello! I am a MELD dataset.")
+    def __init__(self, csv_path, video_dir):
+        self.data = pd.read_csv(csv_path)  # read the csv file
+        print(len(self.data))
 
 
 if __name__ == "__main__":
-    meld = MELDDataset()
+    meld = MELDDataset('/Users/adityamishra/Documents/AI-Sentiment-Analyser/dataset.Raw/dev/dev_sent_emo.csv',
+                       '/Users/adityamishra/Documents/AI-Sentiment-Analyser/dataset.Raw/dev/dev_splits_complete')
