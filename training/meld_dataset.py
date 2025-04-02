4195a1975ae9173b629389b60489c75b114208e0
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-02 19:44:36 +0530

    added length and item retrieval methods to MELDDataset class with video file existence check

--- training/meld_dataset.py
+++ training/meld_dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 import pandas as pd
 from transformers import AutoTokenizer
+import os
 
 
 class MELDDataset(Dataset):
@@ -31,7 +32,25 @@ class MELDDataset(Dataset):
             'positive': 2
         }
 
+    def __len__(self):
+        return len(self.data)  # return the length of the dataset
+
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]  # get the row at the given index
+        video_filename = f"""dia{row['Dialogue_ID']}_utt{
+            row['Utterance_ID']}.mp4"""  # get the video filename from the row with dialogue ID and utterance ID
+
+        # get the path of the video
+        path = os.path.join(self.video_dir, video_filename)
+        video_path = os.path.exists(path)  # check if the video exists
+
+        if video_path == False:
+            raise FileNotFoundError(f"Video file at {path} not found")
+
+        print("File Found")
+
 
 if __name__ == "__main__":
     meld = MELDDataset('/Users/adityamishra/Documents/AI-Sentiment-Analyser/dataset.Raw/dev/dev_sent_emo.csv',
                        '/Users/adityamishra/Documents/AI-Sentiment-Analyser/dataset.Raw/dev/dev_splits_complete')
+    print(meld[0])
