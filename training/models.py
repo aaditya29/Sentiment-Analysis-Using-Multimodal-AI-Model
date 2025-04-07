92649a3892a8dc5002d5ba7f9281bf525fd972ec
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-07 18:34:38 +0530

    added MultimodalSentimentModel class to models.py

--- training/models.py
+++ training/models.py
@@ -5,3 +5,8 @@ from datetime import datetime
 import torch.nn as nn
 import torch
 import os
+
+
+class MultimodalSentimentModel(nn.Module):
+    def __init__(self):
+        super().__init__()  # accessing the parent class constructor using super()
