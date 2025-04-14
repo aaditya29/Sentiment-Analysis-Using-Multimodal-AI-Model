0273b187bc118cbba4e7203e01ebb5fe476c4d25
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-14 22:41:48 +0530

    added count_parameters function to initialize parameter counts for model components

---
+++ training/count_parameters.py
@@ -0,0 +1,12 @@
+from models import MultiModalSentimentModel
+
+
+def count_parameters(model):
+    params_dict = {
+        'text_encoder': 0,
+        'video_encoder': 0,
+        'audio_encoder': 0,
+        'fusion_layer': 0,
+        'emotion_classifier': 0,
+        'sentiment_classifier': 0
+    }
