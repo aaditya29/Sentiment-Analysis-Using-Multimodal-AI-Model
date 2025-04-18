b8029366f8d6b2f06aca5bfb4391e8a3630c2c7e
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-18 12:30:29 +0530

    added environment variable setup for AWS SageMaker and reorganized imports in train.py

--- training/train.py
+++ training/train.py
@@ -1,7 +1,18 @@
-import sys
-import json
 from tqdm import tqdm
-import torch
 import torchaudio
 import argparse
+import torch
+import json
+import sys
 import os
+
+# forAWS SageMaker
+SM_MODEL_DIR = os.environ.get('SM_MODEL_DIR', ".")
+SM_CHANNEL_TRAINING = os.environ.get(
+    'SM_CHANNEL_TRAINING', "/opt/ml/input/data/training")
+SM_CHANNEL_VALIDATION = os.environ.get(
+    'SM_CHANNEL_VALIDATION', "/opt/ml/input/data/validation")
+SM_CHANNEL_TEST = os.environ.get(
+    'SM_CHANNEL_TEST', "/opt/ml/input/data/test")
+
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = "expandable_segments:True"
