6ba96150e0ed20864d37cd622b7d1bb033aa1da1
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-18 12:45:56 +0530

    added argument parsing for training parameters in train.py

--- training/train.py
+++ training/train.py
@@ -16,3 +16,23 @@ SM_CHANNEL_TEST = os.environ.get(
     'SM_CHANNEL_TEST', "/opt/ml/input/data/test")
 
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = "expandable_segments:True"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=20)
+    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--learning-rate", type=float, default=0.001)
+
+    # Data directories
+    parser.add_argument("--train-dir", type=str, default=SM_CHANNEL_TRAINING)
+    parser.add_argument("--val-dir", type=str, default=SM_CHANNEL_VALIDATION)
+    parser.add_argument("--test-dir", type=str, default=SM_CHANNEL_TEST)
+    parser.add_argument("--model-dir", type=str, default=SM_MODEL_DIR)
+
+    return parser.parse_args()
+
+
+def main():
+
+    print("Available audio backends: ")
