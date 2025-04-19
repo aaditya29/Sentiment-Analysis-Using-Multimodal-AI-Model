e6e30b28f22cb42071c31fdb88e1a0db0437b536
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-19 22:37:36 +0530

    added model path definition in inference script for multimodal sentiment analysis

--- deployment/inference.py
+++ deployment/inference.py
@@ -1,8 +1,11 @@
 from models import MultimodalSentimentModel
 import torch
+import os
 
 
 def model_fn(model_dir):
     device = torch.device("cuda" if torch.cuda.is_available()
                           else "cpu")  # Checking if GPU is available
     model = MultimodalSentimentModel().to(device)  # Load the model
+
+    model_path = os.path.join(model_dir, "model.pth")
