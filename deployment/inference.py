6221862f7f461473d916b83e66e8cfc93065ef12
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-20 17:15:04 +0530

    edited inference script to improve model loading and added evaluation mode

--- deployment/inference.py
+++ deployment/inference.py
@@ -56,9 +56,19 @@ def model_fn(model_dir):
                           else "cpu")  # Checking if GPU is available
     model = MultimodalSentimentModel().to(device)  # Load the model
 
-    model_path = os.path.join(model_dir, "model.pth")
+    model_path = os.path.join(model_dir, 'model.pth')
     if not os.path.exists(model_path):
-        model_path = os.path.join(model_dir, 'model.pth')
+        model_path = os.path.join(model_dir, "model", 'model.pth')
         if not os.path.exists(model_path):
             raise FileNotFoundError(
                 "Model file not found in path " + model_path)
+
+    print("Loading model from path: " + model_path)
+    model.load_state_dict(torch.load(
+        model_path, map_location=device, weights_only=True))
+    model.eval()  # Setting the model to evaluation mode
+
+    return {
+        'model': model,
+        'device': device,
+    }
